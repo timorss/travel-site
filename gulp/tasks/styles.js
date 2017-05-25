5c12788d718290990cb1d0599309c5624ff36358
Timor <timorss@gmail.com>
2017-05-25 21:06:23 +0300

    completed desktop and mobile header sttyle

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,10 +5,11 @@ var cssvars = require('postcss-simple-vars'); // set vars
 var nested = require('postcss-nested');// css in css
 var cssImport = require('postcss-import');//import css to another file
 var mixins= require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles',function(){
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
   .on('error', function(errorInfo){
     console.log(errorInfo.toString());
     this.emit('end');
